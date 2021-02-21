547866bfd50672157e40526b9117ebf82d8241ea
chaunceysun82 <chauncey.sun1982@gmail.com>
2021-02-21 13:50:19 -0800

    create login page and login validation

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 from wtform_fields import *
 from models import *
@@ -12,28 +12,35 @@ app.config['SQLALCHEMY_DATABASE_URI']='postgres://zekbeksehdyqxb:2bdd9ad5f34d7b2
 
 db = SQLAlchemy(app)
 
-
 @app.route("/", methods = ['GET', 'POST'])
 def index():
 
     reg_form = RegistrationForm()
 
+    # Update database if validation success
     if reg_form.validate_on_submit():
         username = reg_form.username.data
         password = reg_form.password.data
-
-        # # Check username exists
-        # user_object = User.query.filter_by(username=username).first()
-        # if user_object:
-        #     return "Someone else has taken this username!"
 
         # Add user to database
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
-        return "Insert into Database!"
+        return redirect(url_for('login'))
 
     return render_template("index.html", form=reg_form)
+
+
+@app.route("/login", methods =['GET', 'POST'])
+def login():
+
+    login_form = LoginForm()
+
+    # Allow login if validation success
+    if login_form.validate_on_submit():
+        return "Logged in, finally!"
+
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == '__main__':
